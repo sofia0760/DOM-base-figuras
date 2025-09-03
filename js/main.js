c46ea6fc44c4d49a723b6727c5169bc41aa6f53e
sofia camelo <camelojimenezsofia20@gmail.com>
2025-09-03 15:52:36 -0500

    Agregue una funcion para cambiar la figura a una estrella

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@ function cambiarCirculo(){
     figura.style.background = "purple";
 
 }
-// function cambiarEstrella(){
-//     figura.style.clicPath =
-// }
+function cambiarEstrella(){
+    figura.style.clipPath ="polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+}
 
 
 
 document.addEventListener("DOMContentLoaded",function () {
-    document.getElementById("btnCircle").addEventListener("click",circulo);
+    document.getElementById("btnCircle").addEventListener("click",cambiarCirculo);
+    document.getElementById("btnStar").addEventListener("click",cambiarEstrella);
 });
 
 
