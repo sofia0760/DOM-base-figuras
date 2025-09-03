6e2bd5230c27c4f4d3a0e72efeecd88c2a34a77c
sofia camelo <camelojimenezsofia20@gmail.com>
2025-09-03 16:32:57 -0500

    Agregue una funcion para cambiar la figura para que tenga una animacion

--- js/main.js
+++ js/main.js
@@ -6,10 +6,16 @@ function cambiarCirculo(){
 }
 function cambiarEstrella(){
     figura.style.clipPath ="polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+    figura.style.background = "green";
 }
 function CambiarGif(){
     figura.style.backgroundImage = "url('img/4g.gif')";
     figura.style.backgroundSize = "cover";
+
+}
+function agregarAnimacion() {
+    figura.classList.toggle("parpadear");
+    figura.style.background = "pink";
 }
 
 
@@ -18,6 +24,7 @@ document.addEventListener("DOMContentLoaded",function () {
     document.getElementById("btnCircle").addEventListener("click",cambiarCirculo);
     document.getElementById("btnStar").addEventListener("click",cambiarEstrella);
     document.getElementById("btnGif").addEventListener("click", CambiarGif);
+    document.getElementById("btnAnim").addEventListener("click", agregarAnimacion);
 });
 
 
